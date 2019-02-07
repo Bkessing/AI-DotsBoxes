a769ad8d70ba13365010d0f9823c53dcd5b323be
Brandon <Brandkessinger@gmail.com>
2019-02-07 00:27:17 -0600

    changed GUI plan

--- src/dotandboxes/Board.java
+++ src/dotandboxes/Board.java
@@ -2,62 +2,101 @@ package dotandboxes;
 
 import java.util.ArrayList;
 import java.util.Random;
+import java.util.Scanner;
 
 import javax.swing.JFrame;
 
 public class Board {
 	int[] values;
-	ArrayList<Integer> selections;
-	ArrayList<int[]> connected;
+	String[][] state;
+	ArrayList<int[][]> connected;
 	boolean player1Turn;
 	boolean player2Turn;
-	
-	public Board() {
-		connected = new ArrayList<int[]>();
-		selections = new ArrayList<Integer>();
-		this.values = getRandoms(new int[8]);
-		
-		player1Turn = false;
+	boolean playing;
+	Scanner reader;
+
+	public Board(String[][] startState) {
+		this.state = startState;
+		this.getRandoms(state);
+		this.connected = new ArrayList<int[][]>();
+		this.reader = new Scanner(System.in);
+
+		player1Turn = true;
 		player2Turn = false;
-		this.start();
 		
-	}
-	
-	private void start() {
-		JFrame window = new JFrame();
-		DrawingComponent dc = new DrawingComponent();	
-		window.setSize(430,430);
-		window.setTitle("Dots and Boxes");
-		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		window.add(dc);		
-		window.setVisible(true);
 		
+
 	}
-	
-	public void connect(int index1,int index2) {
-		this.connected.add(new int[]{index1,index2});
+
+	public void connect(String line) {
+		line = line.replaceAll("(","");
+		line = line.replaceAll(")","");
+		line = line.replaceAll(","," ");
+		String[] temp = line.split(",");
+		int index1 = Integer.decode(temp[0]);
+		int index2 = Integer.decode(temp[1]);
+		int index3 = Integer.decode(temp[2]);
+		int index4 = Integer.decode(temp[3]);
 		
+		
+		this.connected.add(new int[][] {{index1, index2},{index3,index4}});
+
 	}
-	
-	private int[] getRandoms(int[] values) {
+
+	private int[] getRandoms(String[][] state) {
 		Random r = new Random();
-		
-		for(int i = 0;i<values.length;i++) {
+
+		for (int i = 0; i < values.length; i++) {
 			values[i] = r.nextInt(4) + 1;
 		}
-		
+
 		return values;
+
+	}
+
+	public void print() {
+
+		for (int i = 0; i < state.length; i++) {
+			for (int j = 0; j < state[0].length; j++) {
+				System.out.print(state[i][j] + " ");
+			}
+			System.out.println(" ");
+		}
+
+	}
+	
+	public void start() {
+		playing = true;
+		while(playing){
+			if(player1Turn) {
+				this.takeTurn("Player1");
 				
+				
+			}
+			else {
+				this.takeTurn("Player2");
+				
+			}
+			
+			
+			
+		}
+		
 		
 		
 	}
 	
-	public void addSelection(int selection) {
-		selections.add(selection);
+	private void takeTurn(String player) {
+		this.print();
+		System.out.println(player +"'s turn");
+		System.out.println("Enter space to put line using format: y,x ");
+		String line = reader.next();
+		
+		
+		
+		
 	}
 	
-	public ArrayList<Integer> getSelection() {
-		return selections;
-	}
+	
 
 }
--- src/dotandboxes/Main.java
+++ src/dotandboxes/Main.java
@@ -5,7 +5,23 @@ package dotandboxes;
 public class Main {
 
 	public static void main(String[] args) {
-		Board b = new Board();
+		String[][] startState = new String[][] {{" ","1","2","3","4","5"},{"1","*","-","*"," ","*"},{"2","|"," "," "," "," "},{"3","*"," ","*"," ","*"},{"4"," "," "," "," "," "},{"5","*"," ","*"," ","*"}};
+		
+		for(int i = 0;i<startState.length;i++) {
+			for(int j = 0;j<startState[0].length;j++) {
+				
+				System.out.print(startState[i][j]+ " ");
+				
+			}
+			System.out.println(" ");
+			
+		}
+		
+		
+		
+		
+		
+		//Board b = new Board();
 		
 	}
 
