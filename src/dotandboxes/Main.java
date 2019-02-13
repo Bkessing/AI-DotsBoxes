33010efc160cfb1bc55b0fbd9409055c7b985d7b
bkessing <bkessing@CDM-TS210WS01.dpu.depaul.edu>
2019-02-13 17:27:15 -0600

    testing functions

--- src/dotandboxes/Main.java
+++ src/dotandboxes/Main.java
@@ -1,20 +1,26 @@
 package dotandboxes;
 
-
+import java.util.ArrayList;
 
 public class Main {
 
 	public static void main(String[] args) {
-		String[][] startState = new String[][] {{" ","1","2","3","4","5"},{"1","*"," ","*"," ","*"},{"2"," "," "," "," "," "},{"3","*"," ","*"," ","*"},{"4"," "," "," "," "," "},{"5","*"," ","*"," ","*"}};
+		//String[][] startState = new String[][] {{" ","1","2","3","4","5"},{"1","*"," ","*"," ","*"},{"2"," "," "," "," "," "},{"3","*"," ","*"," ","*"},{"4"," "," "," "," "," "},{"5","*"," ","*"," ","*"}};
 		
-	
-		
-		
+		String[][] startState = new String[][] {{" ","1","2","3","4","5"},{"1","*","1","*","2","*"},{"2"," "," "," "," "," "},{"3","*","4","*","5","*"},{"4"," "," "," "," "," "},{"5","*"," ","*"," ","*"}};
 		
 		
-		Board b = new Board(startState);
-		b.start();
 		
+		
+		
+		//Board b = new Board(startState);
+		//b.start();
+		
+		AI ai = new AI();
+		
+		Node node = new Node(startState);
+		node.setMax(true);
+		ai.buildTree(node);
 	}
 
 }
